eb5eeca5fcd3a070e4e2c3f010f5355832f14826
Kaique Santos <kags_@live.com>
2022-10-30 10:29:18 -0300

    rushed necessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SourcetextComponent } from './sourcetext/sourcetext.component';
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SourcetextComponent,
+    PreviewComponent
   ],
   imports: [
     BrowserModule
